97ab1c476e0e8208a558469ac1c96d0bcb6d4e11
wolodymyrnosenko <wolodymyrnosenko@gmail.com>
2023-03-10 18:28:54 +0200

    Init

---
+++ Дз сюда/DZ 6 (исключения).py
@@ -0,0 +1,42 @@
+# result = []
+#
+#
+# def divider(a, b):
+#     if a < b:
+#         raise ValueError
+#     if b > 100:
+#         raise IndexError
+#     return a / b
+#
+#
+# data = {10: 2, 2: 5, "123": 4, 18: 0, 8: 4}
+#
+#
+#
+#
+# print(result)
+
+
+
+
+result = []
+
+
+def divider(a, b):
+    if a < b:
+        raise ValueError
+    if b > 100:
+        raise IndexError
+    return a/b
+
+
+data = {10: 2, 2: 5, "123": 4, 18: 0, 8 : 4}
+for key in data:
+    try:
+        res = divider(key, data[key])
+        result.append(res)
+    except Exception as error:
+        print(error)
+
+
+print(result)
